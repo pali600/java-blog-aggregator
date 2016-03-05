75f8934ebdf4c6879ed15174401b0892566a6d10
Pal Kecskemeti <kecskemetip@gmail.com>
2016-03-05 20:26:01 +0100

    Third commit: JPA, Spring Data ...

---
+++ src/main/java/cz/jiripinkas/jba/entity/Role.java
@@ -0,0 +1,44 @@
+package cz.jiripinkas.jba.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Role {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@ManyToMany
+	private List<User> users;
+	private String name;
+	
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void setUsers(List<User> users) {
+		this.users = users;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
