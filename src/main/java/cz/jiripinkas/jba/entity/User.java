c8063c577d97bbaddac88a02a097ad923468c4b9
Pal Kecskemeti <kecskemetip@gmail.com>
2016-03-15 17:37:16 +0100

    Spring web app tutorial 40

--- src/main/java/cz/jiripinkas/jba/entity/User.java
+++ src/main/java/cz/jiripinkas/jba/entity/User.java
@@ -3,6 +3,7 @@ package cz.jiripinkas.jba.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
@@ -14,30 +15,34 @@ import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
 
+import cz.jiripinkas.jba.annotation.UniqueUsername;
+
 @Entity
 public class User {
-	
+
 	@Id
 	@GeneratedValue
 	private Integer id;
-	
-	@Size(min=3, message="Name must be at least 3 characters!")
+
+	@Size(min = 3, message = "Name must be at least 3 characters!")
+	@Column(unique = true)
+	@UniqueUsername(message = "Such username already exists")
 	private String name;
-	
-	@Size(min=1, message="Invalid email address!")
-	@Email(message="Invalid email address!")
+
+	@Size(min = 1, message = "Invalid email address!")
+	@Email(message = "Invalid email address!")
 	private String email;
-	
-	@Size(min=5, message="Password must be at least 5 characters!")
+
+	@Size(min = 5, message = "Password must be at least 5 characters!")
 	private String password;
-	
+
 	@ManyToMany
 	@JoinTable
 	private List<Role> roles;
-	
-	@OneToMany(mappedBy = "user", cascade=CascadeType.REMOVE)
+
+	@OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE)
 	private List<Blog> blogs;
-	
+
 	private boolean enabled;
 
 	public Integer getId() {
