a72531caa5613f869a7e1d79bf6b97918cb2a4d3
Pal Kecskemeti <kecskemetip@gmail.com>
2016-03-16 13:54:10 +0100

    Spring web app tutorial extras

--- src/main/java/cz/jiripinkas/jba/controller/RegisterController.java
+++ src/main/java/cz/jiripinkas/jba/controller/RegisterController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import cz.jiripinkas.jba.entity.User;
 import cz.jiripinkas.jba.service.UserService;
@@ -20,32 +21,33 @@ public class RegisterController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping
 	public String showRegister() {
 		return "user-register";
 	}
-	
+
 	@ModelAttribute("user")
 	public User constructUser() {
 		return new User();
 	}
-	
-	@RequestMapping(method=RequestMethod.POST)
-	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result) {
-		if(result.hasErrors()) {
+
+	@RequestMapping(method = RequestMethod.POST)
+	public String doRegister(@Valid @ModelAttribute("user") User user, BindingResult result,
+			RedirectAttributes redirectAttributes) {
+		if (result.hasErrors()) {
 			return "user-register";
 		}
 		userService.save(user);
-		return "redirect:/register.html?success=true";
+		redirectAttributes.addFlashAttribute("success", true);
+		return "redirect:/register.html";
 	}
-	
+
 	@RequestMapping("/available")
 	@ResponseBody
 	public String available(@RequestParam String username) {
 		Boolean available = userService.findOne(username) == null;
 		return available.toString();
 	}
-	
-	
+
 }
