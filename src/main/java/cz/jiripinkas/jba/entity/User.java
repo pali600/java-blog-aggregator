aca8669260547a8a483c201e9b46231fc7780566
Pal Kecskemeti <kecskemetip@gmail.com>
2016-03-16 13:29:12 +0100

    Spring web app tutorial 51

--- src/main/java/cz/jiripinkas/jba/entity/User.java
+++ src/main/java/cz/jiripinkas/jba/entity/User.java
@@ -5,12 +5,12 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
@@ -18,6 +18,7 @@ import org.hibernate.validator.constraints.Email;
 import cz.jiripinkas.jba.annotation.UniqueUsername;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
 	@Id
